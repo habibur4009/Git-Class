14e6c2c47bb284d5e9baec5200e6f78885914123
habibur4009 <mdhabibur4009@gmail.com>
2024-09-20 01:22:19 +0600

    Adding Independent day of BD

--- FirstClass.java
+++ FirstClass.java
@@ -3,6 +3,8 @@ public class FirstClass {
     public static void main(String[] args) {
 
         String countryName = "Bangladesh";
+        int number = 1971;
+        System.out.println("Independent year of Bangladesh is:" + number);
 
     }
 }
